12c84cdaee525c8df192f2e00f9b821b8159e668
feilongzaitia <A2322146826@163.com>
2024-07-05 19:52:52 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# il2cpp_class_dumper
-一个用于动态dump 魔改il2cpp框架的unity游戏符号的frida脚本
+# 使用方法
+frida-dump-il2cpp-class.js中需要修改il2cpp_api，InitLocked_pianyi，package_name这3个变量的值
+il2cpp_api可通过 [frida-find-il2cpp-api](https://github.com/AndroidReverser-Test/frida-find-il2cpp-api)这个项目中的find-il2cpp-api2.js的注入来获取
+
+InitLocked_pianyi则需要使用ida解析libil2cpp.so后，通过搜索字符串"' because generic types cannot have explicit layout."再查找其交叉引用定位到需要的函数附近
+![class_dump_function](class_dump.JPG)
+上图标黄处的函数地址即为需要的偏移
+
+package_name即为包名
+
+在正确修改上述偏移后即可通过frida spawn启动目标app注入脚本，待需要的class完全加载后，再主动调用get_class_info若无错误发生将会在/data/data/包名/files/下生成
+class_info.log.
---
+++ frida-dump-il2cpp-class.js
@@ -0,0 +1,122 @@
+function hook_call_constructors() {
+    let get_soname = null;
+    let call_constructors_addr = null;
+    let hook_call_constructors_addr = true;
+    let soflags = [];
+ 
+    let linker = null;
+    if (Process.pointerSize == 4) {
+        linker = Process.findModuleByName("linker");
+    } else {
+        linker = Process.findModuleByName("linker64");
+    }
+ 
+    let symbols = linker.enumerateSymbols();
+    for (let index = 0; index < symbols.length; index++) {
+        let symbol = symbols[index];
+        if (symbol.name == "__dl__ZN6soinfo17call_constructorsEv") {
+            call_constructors_addr = symbol.address;
+        } else if (symbol.name == "__dl__ZNK6soinfo10get_sonameEv") {
+            get_soname = new NativeFunction(symbol.address, "pointer", ["pointer"]);
+        }
+    }
+    if (hook_call_constructors_addr && call_constructors_addr && get_soname) {
+        Interceptor.attach(call_constructors_addr,{
+            onEnter: function(args){
+                let soinfo = args[0];
+                let soname = get_soname(soinfo).readCString();
+                if(soname!=null && !soflags.includes(soname)){
+                    if(soname.indexOf("libil2cpp.so")!=-1){
+                        dump_class();
+                    }
+                    soflags.push(soname);
+                }
+            },
+            onLeave:function(ret){
+            }
+        });
+    }
+}
+
+
+
+
+function dump_class(){
+    let il2cpp_base = Process.findModuleByName("libil2cpp.so").base;
+    let InitLocked_addr = il2cpp_base.add(InitLocked_pianyi);
+    let kclazz = NULL;
+    
+    Interceptor.attach(InitLocked_addr,{
+        onEnter:function(args){
+            kclazz = args[0];
+            kclazzs.push(kclazz);
+        }
+    })
+}
+
+function get_class_info(){
+
+    let il2cpp_base = Process.findModuleByName("libil2cpp.so").base;
+    let kclazz,kclazz_name,namespace,method,method_name,method_pointer,param_count,param,param_type,type_name,mstr;
+    const il2cpp_class_get_namespace = new NativeFunction(il2cpp_base.add(il2cpp_api["il2cpp_class_get_namespace"]),"pointer",["pointer"]);
+    const il2cpp_class_get_name = new NativeFunction(il2cpp_base.add(il2cpp_api["il2cpp_class_get_name"]),"pointer",["pointer"]);
+    const il2cpp_class_get_type = new NativeFunction(il2cpp_base.add(il2cpp_api["il2cpp_class_get_type"]),"pointer",["pointer"]);
+    const il2cpp_method_get_name = new NativeFunction(il2cpp_base.add(il2cpp_api["il2cpp_method_get_name"]),"pointer",["pointer"]);
+    const il2cpp_class_get_methods = new NativeFunction(il2cpp_base.add(il2cpp_api["il2cpp_class_get_methods"]),"pointer",["pointer","pointer"]);
+    const il2cpp_method_get_param_count = new NativeFunction(il2cpp_base.add(il2cpp_api["il2cpp_method_get_param_count"]),"int32",["pointer"]);
+    const il2cpp_method_get_param = new NativeFunction(il2cpp_base.add(il2cpp_api["il2cpp_method_get_param"]),"pointer",["pointer","int32"]);
+    const path = "/data/data/"+package_name + "/files/class_info.log";
+    const file = new File(path,"w");
+
+    console.log(kclazzs.length,"class dumping");
+    for(let i=0;i<kclazzs.length;i++){
+        kclazz = kclazzs[i];
+        kclazz_name = il2cpp_class_get_name(kclazz).readCString();
+        namespace = il2cpp_class_get_namespace(kclazz).readCString();
+        file.write("-------"+namespace+"-------"+kclazz_name+"-------\n");
+        const iter = Memory.alloc(Process.pointerSize);
+        method = il2cpp_class_get_methods(kclazz,iter);
+        while(!method.isNull()){
+            mstr = "";
+            method_name = il2cpp_method_get_name(method).readCString();
+            method_pointer = method.readPointer().sub(il2cpp_base);
+            param_count = il2cpp_method_get_param_count(method);
+            mstr += method_name + "(";
+            for(let i=0;i<param_count;i++){
+                param = il2cpp_method_get_param(method,i);
+                param_type = il2cpp_class_get_type(param);
+                type_name = il2cpp_class_get_name(param_type).readCString();
+                mstr += type_name+","
+            }
+            mstr += ")  " + method_pointer+"\n";
+            file.write(mstr);
+            method = il2cpp_class_get_methods(kclazz,iter);
+        }
+        file.write("\n");
+    }
+    file.flush();
+    file.close();
+    console.log("success dump all,save to "+path);
+}
+
+
+function main(){
+    hook_call_constructors();
+}
+ 
+//等游戏完全加载完成后主动调用get_class_info方法即可
+//il2cpp_api,package_name,InitLocked_pianyi需要改动
+var il2cpp_api = {
+    "il2cpp_class_get_methods":0x42b52c8,  
+    "il2cpp_method_get_name":0x42b44bc,    
+    "il2cpp_class_get_name":0x42b4370,     
+    "il2cpp_class_get_type":0x42b43f8,     
+    "il2cpp_class_get_namespace":0x42b4384,
+    "il2cpp_method_get_param":0x42b44d8,   
+    "il2cpp_method_get_param_count":0x42b44d4,
+    };
+var package_name = "com.netease.l22";
+var InitLocked_pianyi = 0x42B8E4C;//搜索because generic types cannot have explicit layout.定位
+
+var kclazzs = [];
+main();
